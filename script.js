b14492902140bfeb9aa10a6745225b47cdf5cfc9
Kate Yeager <95903140+yeakatie@users.noreply.github.com>
2022-09-16 16:15:34 -0700

    9-16-22

---
+++ script.js
@@ -0,0 +1,9 @@
+M.AutoInit();
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.materialboxed');
+    var instances = M.Materialbox.init(elems, options);
+  });
+
+
+  
