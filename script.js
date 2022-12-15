b326d55a0ff8627f9ee8a2f0c2fd37b62306dfab
Kate Yeager <95903140+yeakatie@users.noreply.github.com>
2022-12-15 01:54:22 -0800

    no more materialize

--- script.js
+++
@@ -1,8 +0,0 @@
-M.AutoInit();
-
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.materialboxed');
-    var instances = M.Materialbox.init(elems, options);
-  });
-
-
