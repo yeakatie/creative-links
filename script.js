506a0baeb285144372b4466fe005fa3065b6a2dc
Kate Yeager <95903140+yeakatie@users.noreply.github.com>
2024-01-09 15:08:47 -0800

    added padding and emoji heart randomizer

--- script.js
+++ script.js
@@ -1,7 +1,37 @@
 console.log("Hello World, Page Loaded 🧚‍♂️✨👍");
 
+function getRandomEmoji() {
+  var emojis = [
+    "❤️",
+    "😍",
+    "💖",
+    "😊",
+    "🥰",
+    "🫶",
+    "💜",
+    "💛",
+    "🖤",
+    "💚",
+    "🧡",
+    "😻",
+    "🤍",
+    "🩷",
+    "🩶",
+    "🩵",
+    "💙",
+    "😎",
+    "♥",
+    "♡",
+    "❤",
+  ]; // Add more emojis as needed
+  var randomIndex = Math.floor(Math.random() * emojis.length);
+  return emojis[randomIndex];
+}
+
+// Set a random emoji on page load
+document.getElementById("random-emoji").innerText = getRandomEmoji();
+
 // copyright-year should always be the current year i.e. 2023
 
 const currentYear = new Date().getFullYear();
 document.getElementById("copyright-year").innerHTML = currentYear;
-
